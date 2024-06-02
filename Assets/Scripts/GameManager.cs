89f85b45bb240ed8c85579bb13a8757a37e76bec
João Pamplona <62881476+BeastieBaiter@users.noreply.github.com>
2024-06-02 15:03:27 +0100

    Accept Button disable when no seats available and Bug fixing

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -35,7 +35,7 @@ public class GameManager : MonoBehaviour
     // How much suspicion is gained at the end of the week
     private float suspicion_gain = 0;
 
-    private int people = 0;
+    private int _peopleSaved = 0;
     [HideInInspector] public int seatsAvailableCounter;
 
     private int _weekCounter = 0;
@@ -53,7 +53,6 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         seatsAvailableCounter = seatsAvailablePerWeek;
-        people = PersonManager.Instance.peoplePerDay;
     }
 
     // Update is called once per frame
@@ -116,27 +115,25 @@ public class GameManager : MonoBehaviour
         }
 
         highPrioSaved += highPrioBuffer;
-        numberSaved += people;
+        numberSaved += _peopleSaved;
 
         _suspicionBar.SetSuspicion(suspicion);
-        people = 0;
+        _peopleSaved = 0;
         highPrioBuffer = 0;
         suspicion_gain = 0;
-        Console.WriteLine(_weekCounter);
+        
+        if (_weekCounter >= 1)
+        {
+            UIManager.Instance.ShowConsequences(PersonManager.Instance.consequences);
+        }
     }
 
     public void BoardPerson(int threatLevel, bool highPrio)
     {
-        if (people == seatsAvailableCounter)
-        {
-            Console.WriteLine("Boarding limit exceeded");
-            return;
-        }
-
         float suspicionMultiplier = StateSuspicionMultiplier();
 
         seatsAvailableCounter--;
-        people++;
+        _peopleSaved++;
         if (highPrio) {
             highPrioBuffer++;
         }
@@ -151,7 +148,7 @@ public class GameManager : MonoBehaviour
             suspicion_gain += (threatLevel - 1) * 2.5f * suspicionMultiplier;
         }
 
-
+        UIManager.Instance.acceptButton.gameObject.SetActive(seatsAvailableCounter > 0);
     }
 
     // Changes the amount of seats by change amount
@@ -174,10 +171,6 @@ public class GameManager : MonoBehaviour
 
     public void NewWeek()
     {
-        if (_weekCounter >= 1)
-        {
-            UIManager.Instance.ShowConsequences(PersonManager.Instance.consequences);
-        }
         WeekForwarder();
         var pm = PersonManager.Instance;
         pm.consequences = new List<(string, string)>(); //restart list after end of the week
@@ -188,6 +181,7 @@ public class GameManager : MonoBehaviour
         {
             pm.NewDay();
         }
+        UIManager.Instance.acceptButton.gameObject.SetActive(true);
     }
 
     public void EndWeek()
